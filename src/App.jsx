9d2a527096222a3dc39f7775040a05971da8cc86
Benoit Hubert <benoithubert@gmail.com>
2021-01-04 17:22:03 +0100

    add some props and map

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import Item from './components/Item';
 
+const items = [
+  { id: '1', name: 'Jena', actualDate: '01/09/2002' },
+  { id: '2', name: 'Hercule', actualDate: '01/10/2002' },
+  { id: '3', name: 'Hera', actualDate: '09/10/2009' },
+];
+
 function App() {
-  return (
-    <div className="App">
-      <Item name='Jena' actualDate='01/09/2002'/>
-      <Item name= 'Hercule' actualDate='01/10/2002'/>
-    </div>
-  );
+  const listChevaliers = items.map((item) => (
+    <Item key={item.id} name={item.name} actualDate={item.actualDate} />
+  ));
+  return <div className="App">{listChevaliers}</div>;
 }
 
 export default App;
